0d23afc3bdd51fc0beff3d64994357f5946f6356
Valeria <valerijamiller2001@gmail.com>
2024-04-11 18:59:46 +0200

    Guideline + Portfoio_script

--- src/js/script.js
+++ src/js/script.js
@@ -53,6 +53,54 @@ window.addEventListener('DOMContentLoaded', () => {
     }
     changeBgMain();
 
+
+    //Proects
+
+    const portfolioItem = document.querySelectorAll('[data-item]');
+    const portfolioBtn = document.querySelector('.portfolio_btn-blue');
+    let btnClose = false;
+    
+    portfolioBtn.addEventListener('click', () => {
+    
+        if (btnClose === false){
+            portfolioItem.forEach((item) =>{
+                item.classList.add('show');
+                item.classList.remove('hide');
+                btnClose = true;
+            });
+            portfolioBtn.innerText='Скрыть';
+        }
+        else{
+            portfolioItem.forEach((item) =>{
+                item.classList.add('hide');
+                item.classList.remove('show');
+                btnClose = false;
+            });
+            portfolioBtn.innerText='Показать ещё';
+        }
+    });
+
+
+    //Guideline
+
+
+    function changeGuidelineImg() {
+        const guidelineImg = document.querySelector('.guideline_img');
+        const itemImg = [
+            "url('img/guideline.jpeg')",
+            "url('img/guideline_2.jpeg')"
+        ];
+        let i = 0;
+        function img() {
+            guidelineImg.style.backgroundImage = itemImg[i];
+            i = (i + 1) % itemImg.length;
+            guidelineImg.style.webkitTransition = '0.6s';
+        }
+        setInterval(img, 5000);
+    }
+    changeGuidelineImg();
 });
+
+
 
 
